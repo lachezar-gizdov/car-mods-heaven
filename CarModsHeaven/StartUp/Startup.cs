4061c6f31bd10b8d47b0326ce399b6056837d521
Lucho Gizdov <olebg01@gmail.com>
2017-09-25 17:31:02 +0300

    add base mvc project

---
+++ CarModsHeaven/StartUp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(StartUp.Startup))]
+namespace StartUp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
