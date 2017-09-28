c58bcf9ec349f6dcf8ea6b897b6a8a1fb06e9816
Lucho Gizdov <olebg01@gmail.com>
2017-09-28 22:14:29 +0300

    add data annotations to baseDataModel

--- CarModsHeaven/CarModsHeaven/CarModsHeaven.Data.Models/Abstracts/BaseDataModel.cs
+++ CarModsHeaven/CarModsHeaven/CarModsHeaven.Data.Models/Abstracts/BaseDataModel.cs
@@ -1,13 +1,24 @@
 ﻿using CarModsHeaven.Data.Models.Contracts;
 using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace CarModsHeaven.Data.Models.Abstracts
 {
     public abstract class BaseDataModel : IDeletable, IAuditable
     {
+        public Guid Id { get; set; }
+
+        [Index]
         public bool IsDeleted { get; set; }
-        public DateTime? DeletedOn { get; set; }
+
+        [DataType(DataType.DateTime)]
         public DateTime CreatedOn { get; set; }
+
+        [DataType(DataType.DateTime)]
         public DateTime? ModifiedOn { get; set; }
+
+        [DataType(DataType.DateTime)]
+        public DateTime? DeletedOn { get; set; }
     }
 }
