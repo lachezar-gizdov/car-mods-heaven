de57da81802d9653b0eb57f39e600df98502e08c
Lucho Gizdov <olebg01@gmail.com>
2017-09-28 23:47:29 +0300

    add method for applying createdOn and modifiedOn on data models

--- CarModsHeaven/CarModsHeaven/CarModsHeaven.Data/SqlDbContext.cs
+++ CarModsHeaven/CarModsHeaven/CarModsHeaven.Data/SqlDbContext.cs
@@ -1,6 +1,9 @@
 ﻿using CarModsHeaven.Data.Models;
+using CarModsHeaven.Data.Models.Contracts;
 using Microsoft.AspNet.Identity.EntityFramework;
+using System;
 using System.Data.Entity;
+using System.Linq;
 
 namespace CarModsHeaven.Data
 {
@@ -12,6 +15,32 @@ namespace CarModsHeaven.Data
         }
 
         public IDbSet<Project> Projects { get; set; }
+
+        public override int SaveChanges()
+        {
+            this.ApplyAuditInfoRules();
+            return base.SaveChanges();
+        }
+
+        private void ApplyAuditInfoRules()
+        {
+            foreach (var entry in
+                this.ChangeTracker.Entries()
+                    .Where(
+                        e =>
+                        e.Entity is IAuditable && ((e.State == EntityState.Added) || (e.State == EntityState.Modified))))
+            {
+                var entity = (IAuditable)entry.Entity;
+                if (entry.State == EntityState.Added && entity.CreatedOn == default(DateTime))
+                {
+                    entity.CreatedOn = DateTime.Now;
+                }
+                else
+                {
+                    entity.ModifiedOn = DateTime.Now;
+                }
+            }
+        }
 
         public static SqlDbContext Create()
         {
