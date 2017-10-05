f51e469227f422dcceb6f2cba853d7e0af652a58
Lucho Gizdov <olebg01@gmail.com>
2017-10-06 01:46:14 +0300

    add email sending contact form and some styling here and there

---
+++ CarModsHeaven/CarModsHeaven/CarModsHeaven.Web/Models/Contact/ContactViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CarModsHeaven.Web.Models.Account
+{
+    public class ContactViewModel
+    {
+        [Required]
+        [StringLength(20, MinimumLength = 5)]
+        public string Name { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        public string Subject { get; set; }
+
+        [Required]
+        public string Message { get; set; }
+    }
+}
