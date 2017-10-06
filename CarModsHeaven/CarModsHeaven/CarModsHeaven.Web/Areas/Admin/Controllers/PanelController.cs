84b00dc35d20e59ef08aa3b84e1d3f2c2c1f53e5
Lucho Gizdov <olebg01@gmail.com>
2017-10-06 22:29:48 +0300

    add admin area

---
+++ CarModsHeaven/CarModsHeaven/CarModsHeaven.Web/Areas/Admin/Controllers/PanelController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+
+namespace CarModsHeaven.Web.Areas.Admin.Controllers
+{
+    [Authorize(Roles = "Admin")]
+    public class PanelController : Controller
+    {
+        public ActionResult Index()
+        {
+            return this.View();
+        }
+    }
+}
