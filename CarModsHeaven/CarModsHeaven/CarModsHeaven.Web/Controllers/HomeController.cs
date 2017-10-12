e3c2e10ab99b26a91499b267257250bcf9133500
Lucho Gizdov <olebg01@gmail.com>
2017-10-13 00:49:26 +0300

    add 2 unit tests for home controller

--- CarModsHeaven/CarModsHeaven/CarModsHeaven.Web/Controllers/HomeController.cs
+++ CarModsHeaven/CarModsHeaven/CarModsHeaven.Web/Controllers/HomeController.cs
@@ -1,13 +1,7 @@
 ﻿using Bytes2you.Validation;
-using CarModsHeaven.Data;
 using CarModsHeaven.Services.Contracts;
 using CarModsHeaven.Web.Models.Home;
-using Microsoft.AspNet.Identity;
-using Microsoft.AspNet.Identity.Owin;
-using System;
-using System.IO;
 using System.Linq;
-using System.Web;
 using System.Web.Mvc;
 
 namespace CarModsHeaven.Web.Controllers
