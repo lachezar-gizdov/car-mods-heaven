242fdb6c6ec45df4730f3a89c2526051c6a9c401
Lucho Gizdov <olebg01@gmail.com>
2017-10-13 18:42:42 +0300

    remove unused namespalces

--- CarModsHeaven/CarModsHeaven/CarModsHeaven.Web/Controllers/HomeController.cs
+++ CarModsHeaven/CarModsHeaven/CarModsHeaven.Web/Controllers/HomeController.cs
@@ -1,14 +1,4 @@
-﻿using Bytes2you.Validation;
-using CarModsHeaven.Data;
-using CarModsHeaven.Services.Contracts;
-using CarModsHeaven.Web.Models.Home;
-using Microsoft.AspNet.Identity;
-using Microsoft.AspNet.Identity.Owin;
-using System;
-using System.IO;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 
 namespace CarModsHeaven.Web.Controllers
 {
