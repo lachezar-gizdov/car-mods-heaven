c356f8175a088cceb919d66aa777f3c3aaa6582f
Lucho Gizdov <olebg01@gmail.com>
2017-10-14 18:05:11 +0300

    add project for data tests

---
+++ CarModsHeaven/CarModsHeaven/CarModsHeaven.Data.Tests/UnitOfWorkTests/ConstructorShould.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace CarModsHeaven.Data.Tests.UnitOfWork
+{
+    [TestClass]
+    public class ConstructorShould
+    {
+        [TestMethod]
+        public void ThrowArgumentNullExceptionWhenPassedDbContextIsNull()
+        {
+            Assert.ThrowsException<ArgumentNullException>(() => new UnitOfWork(null));
+        }
+    }
+}
