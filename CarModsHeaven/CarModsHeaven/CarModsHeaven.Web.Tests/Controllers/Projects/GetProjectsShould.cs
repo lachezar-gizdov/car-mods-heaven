0ed0e86f4cfad73dd956afcba25e93964f3d31e1
Lucho Gizdov <olebg01@gmail.com>
2017-10-14 15:31:32 +0300

    add getProjects tests and users controller tests

---
+++ CarModsHeaven/CarModsHeaven/CarModsHeaven.Web.Tests/Controllers/Projects/GetProjectsShould.cs
@@ -0,0 +1,60 @@
+﻿using AutoMapper;
+using CarModsHeaven.Auth.Contracts;
+using CarModsHeaven.Data.Models;
+using CarModsHeaven.Services.Contracts;
+using CarModsHeaven.Web.Controllers;
+using CarModsHeaven.Web.Models.ProjectsList;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Telerik.JustMock;
+using TestStack.FluentMVCTesting;
+
+namespace CarModsHeaven.Web.Tests.Controllers.Projects
+{
+    [TestClass]
+    public class GetProjectsShould
+    {
+        [TestMethod]
+        public void RenderDefaultView()
+        {
+            // Arrange
+            var projectsServiceMock = Mock.Create<IProjectsService>();
+            var usersServiceMock = Mock.Create<IUsersService>();
+            var authMock = Mock.Create<IAuthProvider>();
+            this.InitializeMapper();
+
+            // Act
+            var controller = new ProjectsController(projectsServiceMock, usersServiceMock, authMock);
+
+            //Assert
+            controller
+                .WithCallTo(c => c.Index(null))
+                .ShouldRenderDefaultView();
+        }
+
+        [TestMethod]
+        public void CallProjectsServiceMethodGetAll()
+        {
+            // Arrange
+            var projectsServiceMock = Mock.Create<IProjectsService>();
+            var usersServiceMock = Mock.Create<IUsersService>();
+            var authMock = Mock.Create<IAuthProvider>();
+            this.InitializeMapper();
+
+            // Act
+            var controller = new ProjectsController(projectsServiceMock, usersServiceMock, authMock);
+            controller.Index(null);
+
+            // Assert
+            Mock.Assert(() => projectsServiceMock.GetAll(), Occurs.Once());
+        }
+
+        private void InitializeMapper()
+        {
+            Mapper.Initialize(cfg =>
+                    cfg.CreateMap<Project, ProjectDetailsViewModel>()
+                        .ForMember(viewModel => viewModel.CarBrand,
+                            opt => opt.MapFrom(project => project.CarBrand))
+            );
+        }
+    }
+}
---
+++ CarModsHeaven/CarModsHeaven/CarModsHeaven.Web.Tests/Controllers/Users/IndexShould.cs
@@ -0,0 +1,58 @@
+﻿using AutoMapper;
+using CarModsHeaven.Data.Models;
+using CarModsHeaven.Services.Contracts;
+using CarModsHeaven.Web.Controllers;
+using CarModsHeaven.Web.Models.ProjectsList;
+using CarModsHeaven.Web.Models.UsersList;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Telerik.JustMock;
+using TestStack.FluentMVCTesting;
+
+namespace CarModsHeaven.Web.Tests.Controllers.Users
+{
+    [TestClass]
+    public class IndexShould
+    {
+        [TestMethod]
+        public void RenderDefaultView()
+        {
+            // Arrange
+            var projectsServiceMock = Mock.Create<IProjectsService>();
+            var usersServiceMock = Mock.Create<IUsersService>();
+            this.InitializeMapper();
+
+            // Act
+            var controller = new UsersController(usersServiceMock, projectsServiceMock);
+
+            //Assert
+            controller
+                .WithCallTo(c => c.Index())
+                .ShouldRenderDefaultView();
+        }
+
+        [TestMethod]
+        public void CallUsersServiceMethodGetAll()
+        {
+            // Arrange
+            var projectsServiceMock = Mock.Create<IProjectsService>();
+            var usersServiceMock = Mock.Create<IUsersService>();
+            this.InitializeMapper();
+
+            // Act
+            var controller = new UsersController(usersServiceMock, projectsServiceMock);
+            controller.Index();
+
+            // Assert
+            Mock.Assert(() => usersServiceMock.GetAll(), Occurs.Once());
+        }
+
+        private void InitializeMapper()
+        {
+            Mapper.Initialize(cfg =>
+                    cfg.CreateMap<User, UserViewModel>()
+                        .ForMember(viewModel => viewModel.,
+                            opt => opt.MapFrom(user => user.))
+            );
+        }
+    }
+}
