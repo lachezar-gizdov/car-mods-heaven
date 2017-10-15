fe1659826522f2dc6ddfa354256f0b3ae0f27f02
Lucho Gizdov <olebg01@gmail.com>
2017-10-16 02:24:57 +0300

    add sent emails from contact form to admin panel

--- CarModsHeaven/CarModsHeaven/CarModsHeaven.Web/Models/Contact/ContactViewModel.cs
+++ CarModsHeaven/CarModsHeaven/CarModsHeaven.Web/Models/Contact/ContactViewModel.cs
@@ -1,8 +1,11 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using CarModsHeaven.Data.Models;
+using CarModsHeaven.Web.Infrastructure;
+using System.ComponentModel.DataAnnotations;
+using AutoMapper;
 
 namespace CarModsHeaven.Web.Models.Contact
 {
-    public class ContactViewModel
+    public class ContactViewModel : IMapFrom<ContactEmail>, IHaveCustomMappings
     {
         [Required]
         [Display(Name = "Full Name")]
@@ -18,5 +21,12 @@ namespace CarModsHeaven.Web.Models.Contact
 
         [Required]
         public string Content { get; set; }
+
+        public void CreateMappings(IMapperConfigurationExpression configuration)
+        {
+            configuration.CreateMap<ContactEmail, ContactViewModel>()
+                .ForMember(contactViewModel => contactViewModel.SenderName, cfg => cfg.MapFrom(contactEmail => contactEmail.SenderName))
+                .ForMember(contactViewModel => contactViewModel.SenderEmail, cfg => cfg.MapFrom(contactEmail => contactEmail.SenderEmail));
+        }
     }
 }
