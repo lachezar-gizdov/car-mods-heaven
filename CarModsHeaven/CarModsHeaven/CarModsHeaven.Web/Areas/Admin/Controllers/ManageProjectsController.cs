690a8ad27364a9c0ed99c37508313fe872c236d1
Lucho Gizdov <olebg01@gmail.com>
2017-10-15 12:18:35 +0300

    add managing of users and projects in admin area

---
+++ CarModsHeaven/CarModsHeaven/CarModsHeaven.Web/Areas/Admin/Controllers/ManageProjectsController.cs
@@ -0,0 +1,52 @@
+﻿using AutoMapper.QueryableExtensions;
+using Bytes2you.Validation;
+using CarModsHeaven.Auth.Contracts;
+using CarModsHeaven.Services.Contracts;
+using CarModsHeaven.Web.Models.ProjectsList;
+using CarModsHeaven.Web.Models.UsersList;
+using System;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace CarModsHeaven.Web.Areas.Admin.Controllers
+{
+    [Authorize(Roles = "Admin")]
+    public class ManageProjectsController : Controller
+    {
+        private readonly IProjectsService projectsService;
+        private readonly IAuthProvider authProvider;
+        private readonly string projectsServiceCheck = "Projects service is null";
+        private readonly string authProviderCheck = "Auth provider is null";
+
+        public ManageProjectsController(IProjectsService projectsService, IAuthProvider authProvider)
+        {
+            Guard.WhenArgument(projectsService, this.projectsServiceCheck).IsNull().Throw();
+            Guard.WhenArgument(authProvider, this.authProviderCheck).IsNull().Throw();
+            this.projectsService = projectsService;
+            this.authProvider = authProvider;
+        }
+
+        public ActionResult Index()
+        {
+            var users = this.projectsService
+                .GetAll()
+                .ProjectTo<ProjectDetailsViewModel>()
+                .ToList();
+
+            return this.View(users);
+        }
+
+        public ActionResult DeleteProject(Guid id)
+        {
+            var project = this.projectsService.GetById(id).SingleOrDefault();
+            if (project == null)
+            {
+                return this.View("Error");
+            }
+
+            this.projectsService.Delete(project);
+
+            return this.RedirectToAction("Index");
+        }
+    }
+}
--- CarModsHeaven/CarModsHeaven/CarModsHeaven.Web/Areas/Admin/Controllers/PanelController.cs
+++ CarModsHeaven/CarModsHeaven/CarModsHeaven.Web/Areas/Admin/Controllers/PanelController.cs
@@ -11,50 +11,10 @@ namespace CarModsHeaven.Web.Areas.Admin.Controllers
     [Authorize(Roles = "Admin")]
     public class PanelController : Controller
     {
-        private readonly IUsersService usersService;
-        private readonly IAuthProvider authProvider;
-        private readonly string usersServiceCheck = "Users service is null";
-        private readonly string authProviderCheck = "Auth provider is null";
-
-        public PanelController(IUsersService usersService, IAuthProvider authProvider)
-        {
-            Guard.WhenArgument(usersService, this.usersServiceCheck).IsNull().Throw();
-            Guard.WhenArgument(usersService, this.authProviderCheck).IsNull().Throw();
-            this.usersService = usersService;
-            this.authProvider = authProvider;
-        }
-
         [HttpGet]
         public ActionResult Index()
         {
-            var users = this.usersService
-                .GetAll()
-                .ProjectTo<UserViewModel>()
-                .ToList();
-
-            return this.View(users);
-        }
-
-        [HttpGet]
-        [Authorize(Roles = "Admin")]
-        public ActionResult AddAdmin(string id)
-        {
-            this.authProvider.AddToRole(id, "Admin");
-
-            return this.View("Index");
-        }
-
-        public ActionResult DeleteUser(string id)
-        {
-            var user = this.usersService.GetUserById(id).SingleOrDefault();
-            if (user == null)
-            {
-                return this.View("Error");
-            }
-
-            this.usersService.Delete(user);
-
-            return this.View("Index");
+            return this.View();
         }
     }
 }
